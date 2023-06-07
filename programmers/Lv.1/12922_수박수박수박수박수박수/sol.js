1a50c56c64f877f9d85ae5bd90470322dc75c26f
Skyler <skyler.sy.k@gmail.com>
2023-06-07 15:44:28 +0900

    Add programmers Lv.2 1sol Lv.1 3sol

---
+++ programmers/Lv.1/12922_수박수박수박수박수박수/sol.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12922
+/**
+ * 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+ */
+
+function solution(n) {
+    const dp = Array.from({length : n+1 },()=>"");
+    dp[1] = "수";
+    dp[2] = dp[1] + "박";
+    for(let i=3 ; i<n+1; i++){
+        if(i%2!==0){
+            dp[i] = dp[i-1] + dp[1];
+        }else{
+            dp[i] = dp[i-1] + "박";
+        }
+    }
+   return dp[n];
+}
